edd9f0c6162fecd2c40e9ea6f543904d503f77c0
hinata.o <hinata.o@infratop.jp>
2024-06-10 17:41:21 +0900

    this app finish

--- app/src/features/accordion/index.tsx
+++ app/src/features/accordion/index.tsx
@@ -3,20 +3,27 @@ import {
   AccordionContent,
   AccordionItem,
   AccordionTrigger,
-} from "../../components/ui/accordion"
+} from "@/components/ui/accordion";
 import data from "./data";
 
 const accordion = () => {
-    return (
-      <Accordion type="multiple" className="max-w-96 m-auto bg-orange-700">
+  return (
+    <>
+      <h1 className="text-3xl text-center font-bold my-8">Accordion</h1>
+      <Accordion type="multiple" className="max-w-96 m-auto bg-orange-400">
         {data.map((item) => (
-          <AccordionItem key={item.id} value={`item-${item.id}`} className="p-4">
+          <AccordionItem
+            key={item.id}
+            value={`item-${item.id}`}
+            className="p-4"
+          >
             <AccordionTrigger>{item.question}</AccordionTrigger>
             <AccordionContent>{item.answer}</AccordionContent>
           </AccordionItem>
         ))}
       </Accordion>
-    );
+    </>
+  );
 };
 
 export default accordion;
---
+++ app/src/features/random-color/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const randomColor = () => {
+  return (
+    <>
+      <h1 className="text-3xl text-center font-bold my-8">Random Color</h1>
+      
+    </>
+  );
+}
+
+export default randomColor

4c1d77231df7e9b134de5cfbcf7e2553a149fc39
hinata.o <hinata.o@infratop.jp>
2024-06-10 16:48:38 +0900

    add env, update package.json

--- app/src/features/accordion/index.tsx
+++ app/src/features/accordion/index.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import {
   Accordion,
   AccordionContent,
@@ -9,15 +8,15 @@ import data from "./data";
 
 const accordion = () => {
     return (
-      <Accordion type="single" collapsible className="w-full">
+      <Accordion type="multiple" className="max-w-96 m-auto bg-orange-700">
         {data.map((item) => (
-          <AccordionItem key={item.id} value={`item-${item.id}`}>
+          <AccordionItem key={item.id} value={`item-${item.id}`} className="p-4">
             <AccordionTrigger>{item.question}</AccordionTrigger>
             <AccordionContent>{item.answer}</AccordionContent>
           </AccordionItem>
         ))}
       </Accordion>
-    )
+    );
 };
 
 export default accordion;
